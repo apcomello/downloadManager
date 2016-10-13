ddc36f3cc5f423264f82dac4407ffbb076b88286
Ana Paula Mello <apcomello@gmail.com>
2016-10-13 17:10:34 -0300

    Simple functions added, small changes to UI
    -Folders with non-ascii characters are recognized
    -New buttons added
    -The search function now prints the contents in the text box

--- Forms.py
+++ Forms.py
@@ -1,35 +1,53 @@
 from PyQt4 import QtGui, QtCore
 import sys
+import Manager
 
 class MainWindow:
 
     def __init__(self):
     
-        self.window = QtGui.QWidget()
+        self.window = QtGui.QMainWindow()
+        self.main_widget = QtGui.QWidget()
+        
+        self.window.setCentralWidget(self.main_widget)
         
         self.horizontal_box = QtGui.QHBoxLayout()
         self.first_column = QtGui.QVBoxLayout()
         self.second_column = QtGui.QVBoxLayout()
         self.first_row = QtGui.QHBoxLayout()
         self.second_row = QtGui.QHBoxLayout()
+        self.third_row = QtGui.QHBoxLayout()
         
         self.second_column.addLayout(self.first_row)
         self.second_column.addLayout(self.second_row)
+        self.first_column.addLayout(self.third_row)
         self.horizontal_box.addLayout(self.first_column)
         self.horizontal_box.addLayout(self.second_column)
         self.horizontal_box.setAlignment(QtCore.Qt.AlignRight)
-        self.window.setLayout(self.horizontal_box)
+        self.main_widget.setLayout(self.horizontal_box)
+        
+        self.assistent = Manager.Manager()
     
     def open_window(self):
                 
         self.keyword_search_input = QtGui.QLineEdit()
         self.first_column.addWidget(self.keyword_search_input)
         self.keyword_search_input.setMinimumWidth(300)
-                    
-        search_files_button = QtGui.QPushButton("Search")
-        self.first_column.addWidget(search_files_button)
-        search_files_button.setMaximumWidth(50)
+        
+        search_files_button = QtGui.QPushButton("Set keyword")
+        self.third_row.addWidget(search_files_button)
+        search_files_button.setMaximumWidth(100)
         search_files_button.clicked.connect(self.get_info)
+        
+        run_button = QtGui.QPushButton("Run")
+        self.third_row.addWidget(run_button)
+        run_button.setMaximumWidth(100)
+        run_button.clicked.connect(self.run)
+                    
+        save_button = QtGui.QPushButton("Save preferences")
+        self.third_row.addWidget(save_button)
+        save_button.setMaximumWidth(100)
+        save_button.clicked.connect(self.save)
         
         new_folder_box = QtGui.QCheckBox()
         new_folder_box.setText("Create new folder")
@@ -38,7 +56,11 @@ class MainWindow:
         always_box = QtGui.QCheckBox()
         always_box.setText("Always repeat")
         self.first_column.addWidget(always_box)
-        
+		
+        delete_box = QtGui.QCheckBox()
+        delete_box.setText("Delete")
+        self.first_column.addWidget(delete_box)
+
         rename_box = QtGui.QCheckBox()
         rename_box.setText("Rename files")
         self.first_column.addWidget(rename_box)
@@ -51,6 +73,7 @@ class MainWindow:
         self.first_row.addWidget(self.origin_folder)
         
         self.origin_folder_button = QtGui.QPushButton("...")
+        self.origin_folder_button.clicked.connect(self._set_folder)
         self.first_row.addWidget(self.origin_folder_button)
         
         self.destination_folder = QtGui.QLineEdit()
@@ -60,16 +83,23 @@ class MainWindow:
         self.destination_folder_button = QtGui.QPushButton("...")
         self.second_row.addWidget(self.destination_folder_button)
         
-        all_folders_list = QtGui.QLabel()
-        self.second_column.addWidget(all_folders_list)
-        
-        all_folders_list.resize(50, 100)
-        all_folders_list.setText("Hello, world")
-        all_folders_list.setStyleSheet('background-color: white; padding: 5%;')
+        self.all_folders_list = QtGui.QTextEdit()
+        self.all_folders_list.setReadOnly(True)
+        self.second_column.addWidget(self.all_folders_list)
         
         self.window.move(100, 100)
         self.window.setWindowTitle("Download Manage")
         self.window.show()
         
     def get_info(self):
-        print self.keyword_search_input.text()
+        self.keyword = self.keyword_search_input.text()
+        self.assistent.find_all_files(str(self.keyword), self.all_folders_list)
+        
+    def _set_folder(self):
+        self.assistent.folder = QtGui.QFileDialog.getExistingDirectory(None, "Select folder: ", "C:\Users\Ana Paula Mello\Downloads", QtGui. QFileDialog.ShowDirsOnly)
+        
+    def save(self):
+        pass
+    
+    def run(self):
+        pass
--- Manager.py
+++ Manager.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 
 class Manager:
 
     def __init__(self):
         pass
-        self.default_folder = "C:\Users\Ana Paula Mello\Downloads"
+        self.folder = "C:\Users\Ana Paula Mello\Downloads"
         
     def set_keyword(self, word):
         pass
@@ -15,13 +16,23 @@ class Manager:
     def move_files(self, *file):
         pass
         
-    def find_all_files(self):
-        print os.listdir(os.getcwd())
+    def find_all_files(self, keyword, display):
+        all_files = os.listdir(unicode(self.folder))
+        display.clear()
+        
+        for file in all_files:
+            match_object = re.match(r'(.*)'+keyword.lower()+'(.*)', file.lower())
+            if match_object:
+                display.append(file)
             
     def __create_new_folder(self, folder_name):
         try:
             os.mkdir(folder_name)
         except WindowsError, e:
             print "Folder already exists"
+            
     def __rename(self, pattern):
         pass
+        
+	def __delete(self, pattern):
+		pass
